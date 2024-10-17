38ff14590fe8d597edfdc3c68640d4ff7cec40df
Ozgur Ergin <80709273+kodbutonu@users.noreply.github.com>
2024-10-17 23:59:13 +0300

    added coded

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,28 +1,30 @@
+// Header.jsx
 import { useState } from 'react';
-import { AnimatePresence,motion } from 'framer-motion';
+import { AnimatePresence } from 'framer-motion';
 import NewChallenge from './NewChallenge.jsx';
 
 export default function Header() {
-  const [isCreatingNewChallenge, setIsCreatingNewChallenge] = useState();
+  const [isCreatingNewChallenge, setIsCreatingNewChallenge] = useState(false);
 
   function handleStartAddNewChallenge() {
     setIsCreatingNewChallenge(true);
   }
 
   function handleDone() {
+    console.log("Modal kapandı");
     setIsCreatingNewChallenge(false);
   }
 
   return (
     <>
-    <AnimatePresence>
-      {isCreatingNewChallenge && <NewChallenge onDone={handleDone} />}
+      <AnimatePresence>
+        {isCreatingNewChallenge && <NewChallenge onDone={handleDone} />}
       </AnimatePresence>
       <header id="main-header">
         <h1>Your Challenges</h1>
-        <motion.button onClick={handleStartAddNewChallenge} className="button"  whileHover={{scale:1.1}} transition={{type:'spring',stiffness:500}}>
+        <button onClick={handleStartAddNewChallenge}>
           Add Challenge
-        </motion.button>
+        </button>
       </header>
     </>
   );
